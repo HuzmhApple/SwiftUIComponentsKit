adf66546e50756771548cd85be304a6deebf0b72
Mahmoud Arafat <mahmoud3030arafat@gmail.com>
2026-03-08 17:46:57 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUIComponentsKit",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftUIComponentsKit",
+            targets: ["SwiftUIComponentsKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftUIComponentsKit"),
+        .testTarget(
+            name: "SwiftUIComponentsKitTests",
+            dependencies: ["SwiftUIComponentsKit"]
+        ),
+    ]
+)
